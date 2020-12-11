989b242ae0266dee84fcc95288c6c415e1304090
kevlatus <9928134+kevlatus@users.noreply.github.com>
2020-12-11 20:16:29 +0000

    Deploying to gh-pages from  @ 51db30a31aefac614283bbfb728b3d653167bd79 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,17 +4,17 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "aad657ddd9bceba90b1c1b6dc2b5a85b",
-"main.dart.js": "449f01394e7b00529e2d84b7e546cd33",
-"index.html": "f510073004bd290155b0207c9a300762",
-"/": "f510073004bd290155b0207c9a300762",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
+"assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/NOTICES": "f5aa3b28d38303b4d963409bf8eb813d",
+"main.dart.js": "58340da913e7b941f89bb86c9b0e99be",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"index.html": "7ba6f794bc9137108764fb37e03d697c",
+"/": "7ba6f794bc9137108764fb37e03d697c",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1"
 };
 
 // The application shell files that are downloaded before a service worker can
